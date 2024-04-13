52240d35f854c078d322a9d3dd50e05d17350ec9
Christian Rodrigues <crrodrigues2001@outlook.com.br>
2024-04-12 22:28:35 -0300

    chore: add host env var

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,13 +11,14 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
+  HOST: z.string().default('127.0.0.1'),
   PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  const errorMsg = 'Invalid enviroment variables!'
+  const errorMsg = 'Invalid environment variables!'
   console.error(errorMsg, _env.error.format())
 
   throw new Error(errorMsg)
